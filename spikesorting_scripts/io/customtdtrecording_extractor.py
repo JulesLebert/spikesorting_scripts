1020a54c355f316f06f9c3e9a87be7498ebf434f
JulesLebert <lebert.jules@gmail.com>
2022-12-08 16:49:43 +0000

    remove commented lines

--- spikesorting_scripts/io/customtdtrecording_extractor.py
+++ spikesorting_scripts/io/customtdtrecording_extractor.py
@@ -36,29 +36,4 @@ class CustomTdtRecordingExtractor(NeoBaseRecordingExtractor):
                                            **neo_kwargs)
         self._kwargs.update(dict(folder_path=str(folder_path), stream_id=stream_id, store=store))
 
-
-# class CustomTdtRecordingExtractor(NeoBaseRecordingExtractor):
-#     """
-#     Class for reading TDT folder
-#     Based on neo.rawio.TdTRawIO
-#     Parameters
-#     ----------
-#     folder_path: str
-#         The tdt folder.
-#     stream_id: str or None
-#     """
-#     mode = 'folder'
-#     NeoRawIOClass = 'CustomTdtRawIO'
-
-#     def __init__(self, folder_path, stream_id=None, store=['BB_2','BB_3','BB_4','BB_5']):
-#         neo_kwargs = {'dirname': folder_path, 'store': store}
-#         NeoBaseRecordingExtractor.__init__(self, stream_id=stream_id, **neo_kwargs)
-
-#         self._kwargs = dict(folder_path=str(folder_path), stream_id=stream_id, store=store)
-
-#     @classmethod
-#     def map_to_neo_kwargs(cls, folder_path):
-#         neo_kwargs = {'dirname': str(folder_path)}
-#         return neo_kwargs
-
 read_custom_tdt = define_function_from_class(source_class=CustomTdtRecordingExtractor, name="read_custom_tdt")
